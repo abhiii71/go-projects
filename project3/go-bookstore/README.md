6cb3b042af26c1c2e8e5e88136b4fed8555f7188
Abhishek <abhishek.work71@gmail.com>
2025-07-20 16:40:44 +0530

    done till configuring db

---
+++ project3/go-bookstore/README.md
@@ -0,0 +1,16 @@
+install necessary packages
+gorm for interacting with db 
+➜  go-bookstore git:(project3) ✗ go get github.com/jinzhu/gorm               
+go: downloading github.com/jinzhu/gorm v1.9.16
+go: downloading github.com/jinzhu/inflection v1.0.0
+go: added github.com/jinzhu/gorm v1.9.16
+go: added github.com/jinzhu/inflection v1.0.0
+➜  go-bookstore git:(project3) ✗ 
+
+
+and for sql
+go get github.com/jinzhu/gorm/dialects/mysql                               
+
+
+and mux 
+ go get github.com/gorilla/mux              
---
+++ project3/go-bookstore/pkg/routes/bookstore-route.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/abhiii71/go-bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoute = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
+}
+
+
